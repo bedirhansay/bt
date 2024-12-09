0df7c3fce49a120e96caadccc36b2c376b756194
bedirhan <bedirhan.sayy@nesbilgi.com.tr>
2024-12-09 20:11:53 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 text-6xl pb-20 text-center uppercase gap-16 mt-40 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       ÖZÜR DİLERİM Ballisi bir daha şaka yapmam sana ☺️
       <strong className="uppercase text-4xl">Bu arada yalağuz musun</strong>
+      <strong className="pulse">Bizi kim alabilir tabi yalnız olacaz</strong>
     </div>
   );
 }
