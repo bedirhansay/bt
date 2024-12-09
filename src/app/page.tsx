43b27095a925d827477f5da4590adfa7aa845c7b
bedirhan <bedirhan.sayy@nesbilgi.com.tr>
2024-12-09 20:21:54 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 export default function Home() {
+  const list = Array.from({ length: 20 }, (_, index) => `Item ${index + 1}`);
+
+  console.log(list);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 text-6xl pb-20 text-center uppercase gap-16 mt-40 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      ÖZÜR DİLERİM Ballisi bir daha şaka yapmam sana ☺️
-      <strong className="uppercase text-4xl">Bu arada yalağuz musun</strong>
-      <strong className="pulse">Bizi kim alabilir tabi yalnız olacaz</strong>
+    <div className="items-center justify-items-center min-h-screen p-8 text-6xl pb-20 text-center uppercase gap-16 mt-40 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Kendini hiç bir zaman üzme☺️
+      <ul>
+        {list.map((e) => (
+          <li key={e}>
+            Kimse senin kadar değerli değil
+            <br />
+            <span>Çünkü</span>
+            <br />
+            <span className="animate-pulse">!</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
