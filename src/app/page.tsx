331d7bbd6e3d57a3db30ef218a37c07b8eac34e2
bedirhan <bedirhan.sayy@nesbilgi.com.tr>
2024-12-11 00:36:23 +0300

    ru

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,244 +1,263 @@
 export default function Home() {
   return (
-    <div>
-      <div className="loader">
-        <div className="loader-icon-1">&#10052;</div>
-        <div className="loader-text"></div>
-      </div>
-      <div className="main">
-        <div className="initial-snow w-full">
-          <div className="mx-auto my-8 p-6 w-full rounded-lg shadow-lg text-center text-lg italic text-gray-700 max-w-lg leading-relaxed">
-            <br /> When a person does their very best, no matter the outcome, their conscience finds peace.
-          </div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-          <div className="snow">&#10052;</div>
-        </div>
-      </div>
+    // <div>
+    //   <div className="loader">
+    //     <div className="loader-icon-1">&#10052;</div>
+    //     <div className="loader-text"></div>
+    //   </div>
+    //   <div className="main">
+    //     <div className="initial-snow w-full">
+    //       <div className="mx-auto my-8 p-6 w-full rounded-lg shadow-lg text-center text-lg italic text-gray-700 max-w-lg leading-relaxed">
+    //         <br /> When a person does their very best, no matter the outcome, their conscience finds peace.
+    //       </div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //       <div className="snow">&#10052;</div>
+    //     </div>
+    //   </div>
 
-      <div className="snowMan">
-        <div className="innerSnow">
-          <div className="snowBall"></div>
-          <div className="snowBall"></div>
-          <div className="snowBall"></div>
-        </div>
-      </div>
-      <div className="snowMan noBlur">
-        <div className="innerSnow">
-          <div className="snowBall"></div>
-          <div className="snowBall"></div>
-          <div className="snowBall"></div>
-        </div>
-      </div>
-      <div className="snowFace">
-        <div className="eye"></div>
-        <div className="eye"></div>
-        <div className="nose"></div>
-        <div className="twigWrapper">
-          <div className="twig shade"></div>
-          <div className="twig"></div>
-          <div className="twigi"></div>
-        </div>
-      </div>
-      <div className="snowFall">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="snowFall delayMe">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="snowFall biggerSnow">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="snowFall delayMe biggerSnow">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    //   <div className="snowMan">
+    //     <div className="innerSnow">
+    //       <div className="snowBall"></div>
+    //       <div className="snowBall"></div>
+    //       <div className="snowBall"></div>
+    //     </div>
+    //   </div>
+    //   <div className="snowMan noBlur">
+    //     <div className="innerSnow">
+    //       <div className="snowBall"></div>
+    //       <div className="snowBall"></div>
+    //       <div className="snowBall"></div>
+    //     </div>
+    //   </div>
+    //   <div className="snowFace">
+    //     <div className="eye"></div>
+    //     <div className="eye"></div>
+    //     <div className="nose"></div>
+    //     <div className="twigWrapper">
+    //       <div className="twig shade"></div>
+    //       <div className="twig"></div>
+    //       <div className="twigi"></div>
+    //     </div>
+    //   </div>
+    //   <div className="snowFall">
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //   </div>
+    //   <div className="snowFall delayMe">
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //   </div>
+    //   <div className="snowFall biggerSnow">
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //   </div>
+    //   <div className="snowFall delayMe biggerSnow">
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //   </div>
 
-      <section className="sleigh">
-        <div className="santa">🦌</div>
-        <div className="santa1">🦌</div>
-        <div className="santa2">🦌</div>
-        <div className="santa3">🦌</div>
-        <div className="santa4">🦌</div>
-        <div className="santa5">
-          <i className="fa-solid fa-sleigh"></i>
-        </div>
-      </section>
-      <section className="snow">
-        <div className="snowflake">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake1">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake2">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake3">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake4">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake5">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake6">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake7">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake8">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake9">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake10">
-          <i className="fa-solid fa-snowflake"></i>
-        </div>
-        <div className="snowflake11">
-          <i className="fa-solid fa-snowflake"></i>
-        </div>
-        <div className="snowflake12">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake13">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-        <div className="snowflake14">
-          <i className="fa-regular fa-snowflake"></i>
-        </div>
-      </section>
-      <section className="mountains">
-        <div className="mt1">
-          <div className="mtsnow1"></div>
-          <div className="snow-peak-1a"></div>
-          <div className="snow-peak-1b"></div>
-        </div>
-        <div className="mt2">
-          <div className="snow2"></div>
-          <div className="snow-peak-2a"></div>
-          <div className="snow-peak-2b"></div>
-          <div className="snow-peak-2c"></div>
-        </div>
-        <div className="mt3">
-          <div className="snow3"></div>
-          <div className="snow-peak-3a"></div>
-          <div className="snow-peak-3b"></div>
-          <div className="snow-peak-3c"></div>
-          <div className="snow-peak-3d"></div>
-        </div>
-        <div className="mt4">
-          <div className="snow4"></div>
-          <div className="snow-peak-4a"></div>
-          <div className="snow-peak-4b"></div>
-        </div>
-        <div className="mt5">
-          <div className="snow5"></div>
-          <div className="snow-peak-5a"></div>
-          <div className="snow-peak-5b"></div>
-          <div className="snow-peak-5c"></div>
-        </div>
-        <div className="mt6">
-          <div className="snow6"></div>
-          <div className="snow-peak-6a"></div>
-          <div className="snow-peak-6b"></div>
-          <div className="snow-peak-6c"></div>
-          <div className="snow-peak-6d"></div>
-        </div>
-      </section>
+    //   <section className="sleigh">
+    //     <div className="santa">🦌</div>
+    //     <div className="santa1">🦌</div>
+    //     <div className="santa2">🦌</div>
+    //     <div className="santa3">🦌</div>
+    //     <div className="santa4">🦌</div>
+    //     <div className="santa5">
+    //       <i className="fa-solid fa-sleigh"></i>
+    //     </div>
+    //   </section>
+    //   <section className="snow">
+    //     <div className="snowflake">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake1">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake2">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake3">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake4">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake5">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake6">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake7">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake8">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake9">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake10">
+    //       <i className="fa-solid fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake11">
+    //       <i className="fa-solid fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake12">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake13">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //     <div className="snowflake14">
+    //       <i className="fa-regular fa-snowflake"></i>
+    //     </div>
+    //   </section>
+    //   <section className="mountains">
+    //     <div className="mt1">
+    //       <div className="mtsnow1"></div>
+    //       <div className="snow-peak-1a"></div>
+    //       <div className="snow-peak-1b"></div>
+    //     </div>
+    //     <div className="mt2">
+    //       <div className="snow2"></div>
+    //       <div className="snow-peak-2a"></div>
+    //       <div className="snow-peak-2b"></div>
+    //       <div className="snow-peak-2c"></div>
+    //     </div>
+    //     <div className="mt3">
+    //       <div className="snow3"></div>
+    //       <div className="snow-peak-3a"></div>
+    //       <div className="snow-peak-3b"></div>
+    //       <div className="snow-peak-3c"></div>
+    //       <div className="snow-peak-3d"></div>
+    //     </div>
+    //     <div className="mt4">
+    //       <div className="snow4"></div>
+    //       <div className="snow-peak-4a"></div>
+    //       <div className="snow-peak-4b"></div>
+    //     </div>
+    //     <div className="mt5">
+    //       <div className="snow5"></div>
+    //       <div className="snow-peak-5a"></div>
+    //       <div className="snow-peak-5b"></div>
+    //       <div className="snow-peak-5c"></div>
+    //     </div>
+    //     <div className="mt6">
+    //       <div className="snow6"></div>
+    //       <div className="snow-peak-6a"></div>
+    //       <div className="snow-peak-6b"></div>
+    //       <div className="snow-peak-6c"></div>
+    //       <div className="snow-peak-6d"></div>
+    //     </div>
+    //   </section>
+    // </div>
+    <div className="bg-black text-white p-8 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold text-yellow-400 uppercase mb-4">Hayat Kuralı No:12</h2>
+      <strong className="block text-lg text-orange-500 mb-4">Seneca’nın 25. Mektubu</strong>
+      <p className="text-base leading-relaxed text-gray-300">
+        “Fakat hiçbir şey insanın dikkat çekmeyecek şekilde davranması ve başkalarıyla pek az, kendisiyle ise bol bol
+        konuşması kadar fayda sağlamaz.
+        <br />
+        Sohbetin baştan çıkarıcı bir yanı vardır, gizlice sokulup yanaşır ve sarhoşluk sırrı açığa çıkarır. Hiç kimse,
+        duyduğu şeyi, kendine saklayamaz. Meselenin saklanamayacak biri, meselenin sır olan bir şey olduğu bilgisiyle
+        herkesi rahatsız eder. Gevezeliğine nihayetinde gem vursa bile, her durumda güvenilirlikten uzak biri olarak
+        görülmeye mahkumdur.”
+      </p>
+
+      <strong className="block mt-10 text-orange-400 py-4 border-t border-gray-600">
+        -------- Aklı Başında olan kişi mutluluğun değil acı vermeyenin peşindedir.
+      </strong>
+
+      <strong className="block text-sm text-gray-500 italic mt-6">Günaydın Ballisi</strong>
     </div>
   );
 }
